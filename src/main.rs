289de2a7ebfa0cfe091b4ae1662c0e46d4851c84
O2s0k0i3 <elultimopekka@gmail.com>
2024-02-09 16:49:05 +0100

    Preparing for crates.io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//! Welcome to CliFerrilab!
+//!
+//! This cli tool help you create and manage data along with the [FerriLab] library.
+//! 
+//! [FerriLab]: https://github.com/asaltanubes/FerriLab
+//! 
+//! ## Usage
+//! 
+//! Once you've installed the crate you only need to type "ferris" in your terminal
+//! to make it work.
+
 mod cli;
 mod config;
 use cli::*;
@@ -25,7 +36,7 @@ fn main() {
         }
         Commands::Update => {
             std::process::Command::new("cargo")
-                .args(["install", "cli_ferrilab"])
+                .args(["install", "cli-ferrilab"])
                 .spawn()
                 .expect("Failed to update.");
         }
